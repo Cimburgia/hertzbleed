82794e1e69f13edf99e4535a0b85e86c8399e0d6
Carina Imburgia <imburgia@cs.washington.edu>
2023-08-07 18:47:30 -0700

    added sample function

--- util/freq-utils-m.h
+++ util/freq-utils-m.h
@@ -5,23 +5,31 @@
 #include <IOKit/IOKitLib.h>
 #include <CoreFoundation/CoreFoundation.h>
 
+enum {
+    kIOReportIterOk,
+    kIOReportIterFailed,
+    kIOReportIterSkipped
+};
+
 typedef struct IOReportSubscriptionRef* IOReportSubscriptionRef;
 typedef CFDictionaryRef IOReportSampleRef;
 
-struct unit_data{
+typedef struct unit_data{
     IOReportSubscriptionRef cpu_sub;
     CFMutableDictionaryRef cpu_sub_chann;
     CFMutableDictionaryRef cpu_chann;
-};
+} unit_data;
 
 extern IOReportSubscriptionRef IOReportCreateSubscription(void* a, CFMutableDictionaryRef desiredChannels, CFMutableDictionaryRef* subbedChannels, uint64_t channel_id, CFTypeRef b);
 extern CFDictionaryRef IOReportCreateSamples(IOReportSubscriptionRef iorsub, CFMutableDictionaryRef subbedChannels, CFTypeRef a);
 extern CFDictionaryRef IOReportCreateSamplesDelta(CFDictionaryRef prev, CFDictionaryRef current, CFTypeRef a);
+extern CFMutableDictionaryRef IOReportCopyChannelsInGroup(CFStringRef, CFStringRef, uint64_t, uint64_t, uint64_t);
 typedef int (^ioreportiterateblock)(IOReportSampleRef ch);
 extern void IOReportIterate(CFDictionaryRef samples, ioreportiterateblock);
+extern int IOReportStateGetCount(CFDictionaryRef);
 
 void init_unit_data(unit_data *data);
-void sample(int time);
-uint64_t* get_state_res(int core_id, int time);
-uint64_t get_frequency(int core_id);
+void sample(unit_data *data, int time);
+// uint64_t* get_state_res(int core_id, int time);
+// uint64_t get_frequency(int core_id);
 #endif
