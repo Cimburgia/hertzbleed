f498e9369e47d9cc100354132cff900db66363f1
Carina Imburgia <imburgia@cs.washington.edu>
2023-08-21 22:10:10 -0700

    added run script with comment

--- 03-leakage-model-hd-m2/run_model.sh
+++ 03-leakage-model-hd-m2/run_model.sh
@@ -6,11 +6,25 @@ TOTAL_LOGICAL_CORES=`sysctl -n hw.logicalcpu `
 # Load MSR module
 #sudo modprobe msr
 
+# Check for entering a comment to run
+if [ "$#" -ne 1 ]; then
+    echo "Single Comment needed..."
+    exit 1
+fi
+
+# Create and save new run folder
+date=`date +"%m%d-%H%M"`
+new_dir="data/out-$date"
+mkdir -p "$new_dir"
+
+# Compile and execute program, save stdout to log file
+echo "$1" > "$new_dir/log.txt"
+
 # Setup
 samples=10000	# 10 seconds
-outer=5
+outer=3
 num_thread=$TOTAL_LOGICAL_CORES
-date=`date +"%m%d-%H%M"`
+
 
 # Alert
 echo "This script will take about $((((10)*$outer*(16+56+48+32+16+56+48+32+16+16)/60+10)/60)) hours. Reduce 'outer' if you want a shorter run."
@@ -26,5 +40,5 @@ for selector in `seq 0 16`; do
 	echo $selector >> input.txt
 done
 
-sudo ./bin/driver ${num_thread} ${samples} ${outer}
-cp -r out data/out-${date}
+sudo ./bin/driver ${num_thread} ${samples} ${outer} >> "$new_dir/log.txt"
+cp -r out $new_dir/out-${date}
